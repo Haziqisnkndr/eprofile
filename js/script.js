119c7772e8acd18bf39c2af2acfca6bb6c712444
Haziqisnkndr <haziqisnkandar00@gmail.com>
2025-01-03 16:40:38 +0800

    First Commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Add active class to clicked nav item
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function () {
+        document.querySelectorAll('.nav-link').forEach(item => {
+            item.classList.remove('active');
+        });
+        this.classList.add('active');
+    });
+});
